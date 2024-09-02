bfa3700f14aa370109e85f32b17dc240bddefbfd
Piyush Gupta <94291113+piyushh-guptaa@users.noreply.github.com>
2024-09-02 20:16:22 +0530

    Time: 103 ms (26.78%), Space: 77.1 MB (8.71%) - LeetHub

---
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int chalkReplacer(vector<int>& chalk, int k) {
+        long long total = 0;
+        for (int c : chalk) {
+            total += c;
+        }
+
+        k = k % total;
+
+        for (int i = 0; i < chalk.size(); i++) {
+            if (k < chalk[i]) {
+                return i;
+            }
+            k -= chalk[i];
+        }
+
+        return -1;
+    }
+};
