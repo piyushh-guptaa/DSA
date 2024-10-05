0a8191ccc8388d168453e0982cc3109a5fb4a5cd
Piyush Gupta <94291113+piyushh-guptaa@users.noreply.github.com>
2024-10-05 23:20:51 +0530

    Time: 11 ms (38.85%), Space: 10.7 MB (28.55%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,18 +1,33 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+using namespace std;
+
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int size = s1.length();
-        unordered_map<char, int> map;
-        for(int i = 0; i < s1.length(); i++){
-            map[s1[i]]++;
+        if (s1.length() > s2.length()) {
+            return false;
         }
-        for(int i = 0; i < s2.length(); i++){
-            string curr = s2.substr(i,size);
-            unordered_map<char, int> intMap;
-            for(int j = 0; j < curr.length(); j++){
-                intMap[curr[j]]++;
+        unordered_map<char, int> s1Map, windowMap;
+        for (char c : s1) {
+            s1Map[c]++;
+        }
+        int windowSize = s1.length();
+        for (int i = 0; i < windowSize; i++) {
+            windowMap[s2[i]]++;
+        }
+        if (s1Map == windowMap) {
+            return true;
+        }
+        for (int i = windowSize; i < s2.length(); i++) {
+            windowMap[s2[i]]++;
+            char charToRemove = s2[i - windowSize];
+            windowMap[charToRemove]--;
+            if (windowMap[charToRemove] == 0) {
+                windowMap.erase(charToRemove);
             }
-            if(map == intMap){
+            if (s1Map == windowMap) {
                 return true;
             }
         }
