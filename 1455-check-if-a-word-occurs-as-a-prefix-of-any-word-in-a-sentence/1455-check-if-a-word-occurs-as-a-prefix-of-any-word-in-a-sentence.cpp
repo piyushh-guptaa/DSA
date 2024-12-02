b0edbf4e5b9153a28d0df464df1eb643e304cd41
Piyush Gupta <94291113+piyushh-guptaa@users.noreply.github.com>
2024-12-02 16:05:25 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (7.80%) - LeetHub

--- 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     int isPrefixOfWord(string sentence, string searchWord) {
-        vector<string> words;
-        int start = 0;
-        int wordCount = 0;
-        for (int i = 0; i <= sentence.size(); i++) {
-            if (i == sentence.size() || sentence[i] == ' ') { 
-                words.push_back(sentence.substr(start, i - start)); 
-                start = i + 1;
-                wordCount++;
-            }
-        }
-        for(int i = 0; i < words.size(); i++){
-            string word = words[i];
-            if(searchWord[0] == word[0]){
-                int n = searchWord.length();
-                if(word.substr(0,n) == searchWord){
-                    return i+1;
+        int wordIndex = 1;
+        int i = 0, j = 0;
+
+        while (i < sentence.size()) {
+            if (sentence[i] == ' ') {
+                wordIndex++;
+                i++;
+                j = 0;
+            } else if (j < searchWord.size() && sentence[i] == searchWord[j]) {
+                j++;
+                i++;
+                if (j == searchWord.size()) {
+                    return wordIndex;
                 }
+            } else {
+                while (i < sentence.size() && sentence[i] != ' ') {
+                    i++;
+                }
+                j = 0;
             }
         }
-
-
-    return -1;
+        return -1;
     }
 };
